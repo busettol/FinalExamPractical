dd5971433bb5e64e81f89b7af8d5482f4d19da98
Lucas Busetto <lbusetto99@live.com>
2024-12-05 09:19:33 -0500

    Initial Commit

---
+++ FinalExam/FinalExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalExamApp.swift
+//  FinalExam
+//
+//  Created by Lucas Busetto on 2024-12-05.
+//
+
+import SwiftUI
+
+@main
+struct FinalExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
